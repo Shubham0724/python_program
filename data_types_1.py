dd9fac0583905c33e368946dae27804d95c919c3
Shubham Bodkhe <109900001+Shubham0724@users.noreply.github.com>
2023-01-29 05:10:54 -0800

    Add files via upload

---
+++ data_types_1.py
@@ -0,0 +1,19 @@
+# Sequenced data
+
+#list
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+#tuple
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+#range
+sequence1 = range(4,14,2)
+for i in sequence1:
+    print(i)
+
+#Mapped data: dict
+
+dict1 = {"name":"Central", "age":20, "canVote":True}
+print(dict1)
