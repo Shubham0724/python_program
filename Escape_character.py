fb0cc8f161de91ebbc1b01dbb9d6923487e07551
Shubham Bodkhe <109900001+Shubham0724@users.noreply.github.com>
2023-01-31 19:46:27 -0800

    Add files via upload

---
+++ Escape_character.py
@@ -0,0 +1,15 @@
+#New Line:
+str1 = "I love doing Yoga. \nIt cleanses my mind and my body."
+print(str1)
+
+#Tab:
+str2 = "Central \t\tCee \t!!!"
+print(str2)
+
+#Backspace
+str2 = "Hello  \bWorld !!!"
+print(str2)
+
+#Backslash
+str3 = "What will you eat? Apple\\Banana"
+print(str3)
