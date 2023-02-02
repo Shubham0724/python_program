b4205e8c94a79a5cb03b4d8ad41c752f100f4faf
Shubham Bodkhe <109900001+Shubham0724@users.noreply.github.com>
2023-02-02 00:12:44 -0800

    Add files via upload

---
+++ format_strings.py
@@ -0,0 +1,16 @@
+# concatenation 
+
+#Example :
+
+str4 = "Captain"
+str5 = "America"
+str6 = str4 + " " + str5
+print(str6)
+
+# Example :
+quantity = 2
+fruit = "Apples"
+cost = 120.0
+statement = "I want to buy {2} dozen {0} for {1}$"
+print(statement.format(fruit,cost,quantity))
+
